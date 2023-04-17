dbf33873232a103705ece183c7213b13a2892181
Massimo Fasciano <massimo@mafalitic.com>
2023-04-17 18:20:57 -0400

    resolve conflicts and remove dead units

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,34 @@ impl Default for Game {
     }
 }
 
+pub trait SliceExt {
+    type Item;
+
+    fn get_two_mut(&mut self, index0: usize, index1: usize) -> (&mut Self::Item, &mut Self::Item);
+}
+
+impl<T> SliceExt for [T] {
+    type Item = T;
+
+    fn get_two_mut(&mut self, index0: usize, index1: usize) -> (&mut Self::Item, &mut Self::Item) {
+        match index0.cmp(&index1) {
+            std::cmp::Ordering::Less => {
+                let mut iter = self.iter_mut();
+                let item0 = iter.nth(index0).unwrap();
+                let item1 = iter.nth(index1 - index0 - 1).unwrap();
+                (item0, item1)
+            }
+            std::cmp::Ordering::Equal => panic!("[T]::get_two_mut(): received same index twice ({})", index0),
+            std::cmp::Ordering::Greater => {
+                let mut iter = self.iter_mut();
+                let item1 = iter.nth(index1).unwrap();
+                let item0 = iter.nth(index0 - index1 - 1).unwrap();
+                (item0, item1)
+            }
+        }
+    }
+}
+
 impl Game {
     pub fn get_cell(&self, row: i8, col: i8) -> Option<&Cell> {
         if Self::is_valid_position(row,col) {
@@ -30,6 +58,19 @@ impl Game {
     pub fn get_cell_mut(&mut self, row: i8, col: i8) -> Option<&mut Cell> {
         if Self::is_valid_position(row,col) {
             Some(&mut self.board[row as usize*BOARD_DIM as usize+col as usize])
+        } else {
+            None
+        }
+    }
+    pub fn get_cells_mut(&mut self, cell0: (i8,i8), cell1: (i8,i8)) -> Option<(&mut Cell, &mut Cell)> {
+        if Self::is_valid_position(cell0.0,cell0.1) &&
+            Self::is_valid_position(cell1.0,cell1.1) &&
+            cell0 != cell1
+        {
+            Some(self.board.get_two_mut(
+                cell0.0 as usize*BOARD_DIM as usize+cell0.1 as usize,
+                cell1.0 as usize*BOARD_DIM as usize+cell1.1 as usize)
+            )
         } else {
             None
         }
@@ -60,14 +101,18 @@ impl Game {
         }
     }
     pub fn is_valid_move(&mut self, from: (i8,i8), to: (i8,i8)) -> bool {
-        if Self::is_valid_position(to.0, to.1)
+        Self::is_valid_position(to.0, to.1)
             && Self::is_valid_position(from.0, from.1)
-            && self[to].is_empty() && self[from].is_unit() {
-            ((from.0 - to.0).abs() == 1 && (from.1 == to.1)) ||
-            ((from.1 - to.1).abs() == 1 && (from.0 == to.0))
-        } else {
-            false
-        }
+            && self[to].is_empty() && self[from].is_unit() 
+            && Self::neighbors(from, to) &&
+            self.player() == self[from].player().unwrap()
+    }
+    pub fn neighbors(cell0 : (i8,i8), cell1 : (i8,i8)) -> bool {
+        Self::is_valid_position(cell0.0,cell0.1) &&
+        Self::is_valid_position(cell1.0,cell1.1) && (
+            ((cell1.0 - cell0.0).abs() == 1 && (cell1.1 == cell0.1)) ||
+            ((cell1.1 - cell0.1).abs() == 1 && (cell1.0 == cell0.0))
+        )
     }
     pub fn move_unit(&mut self, from: (i8,i8), to: (i8,i8)) -> bool {
         if self.is_valid_move(from, to) {
@@ -76,6 +121,43 @@ impl Game {
             true
         } else {
             false
+        }
+    }
+    pub fn remove_dead(&mut self) {
+        for cell in self.board.iter_mut() {
+            if let Some((_, unit)) = cell.unit_mut() {
+                if unit.health == 0 {
+                    *cell = Cell::Empty;
+                }
+            }
+        }
+    }
+    pub fn resolve_conflicts(&mut self) {
+        for row in 0..BOARD_DIM {
+            for col in 0..BOARD_DIM {
+                if self[(row,col)].is_unit() {
+                    // println!("unit at {} {}",row,col);
+                    for (rd, cd) in [(-1,0),(1,0),(0,-1),(0,1)] {
+                        let row_target = row + rd;
+                        let col_target = col + cd; 
+                        if Self::is_valid_position(row_target,col_target) 
+                            && self[(row_target,col_target)].is_unit() 
+                        {
+                            // println!("target unit at {} {}",row_target,col_target);
+                            let (source, target) = self.get_cells_mut((row,col), (row_target,col_target)).unwrap();
+                            let (ps,us) = source.unit_mut().unwrap();
+                            let (pt,ut) = target.unit_mut().unwrap();
+                            if ps != pt {
+                                // opponents
+                                us.apply_damage(ut);
+                            } else {
+                                // friends
+                                us.apply_repair(ut);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -130,10 +212,10 @@ impl std::fmt::Display for UnitType {
         write!(f, "{}", match self {
             AI => "A",
             Hacker => "H",
-            Support => "S",
+            Repair => "R",
             Tank => "T",
             Drone => "D",
-            Android => "A",
+            Soldier => "S",
         })
     }
 }
@@ -190,6 +272,24 @@ impl Cell {
             _ => false,
         }
     }
+    pub fn player(&self) -> Option<Player> {
+        match self {
+            Self::Unit { player, unit: _ } => Some(*player),
+            _ => None,
+        }
+    }
+    pub fn unit(&self) -> Option<(&Player,&Unit)> {
+        match self {
+            Self::Unit { player, unit } => Some((player,unit)),
+            _ => None,
+        }
+    }
+    pub fn unit_mut(&mut self) -> Option<(&mut Player,&mut Unit)> {
+        match self {
+            Self::Unit { player, unit } => Some((player,unit)),
+            _ => None,
+        }
+    }
 }
 
 type Health = u8;
@@ -204,11 +304,11 @@ pub struct Unit {
 pub enum UnitType {
     AI,
     Hacker,
-    Support,
+    Repair,
     Tank,
     Drone,
     #[default]
-    Android,
+    Soldier,
 }
 
 impl Default for Unit {
@@ -221,27 +321,43 @@ impl Unit {
     pub fn new(unit_type : UnitType) -> Self {
         use UnitType::*;
         let health = match unit_type {
-            AI => 3,
-            Hacker => 1,
-            Support => 2,
-            Tank => 1,
-            Drone => 1,
-            Android => 1,
+            AI => 5,
+            Hacker => 3,
+            Repair => 2,
+            Tank => 9,
+            Drone => 6,
+            Soldier => 4,
         };
         Self { unit_type, health }
+    }
+    pub fn apply_repair(&mut self, target: &mut Self) {
+        let repair = self.repair(target);
+        if repair + target.health < 9 {
+            target.health += repair;
+        } else {
+            target.health = 9;
+        }
     }
     pub fn repair(&self, target: &Self) -> Health {
         use UnitType::*;
         match self.unit_type {
-            Support => match target.unit_type {
+            Repair => match target.unit_type {
                 AI => 3,
                 Hacker => 1,
-                Support => 2,
+                Repair => 2,
                 Tank => 1,
                 Drone => 1,
-                Android => 1,
+                Soldier => 1,
             },
             _ => 0,
+        }
+    }
+    pub fn apply_damage(&mut self, target: &mut Self) {
+        let damage = self.damage(target);
+        if damage < target.health {
+            target.health -= damage;
+        } else {
+            target.health = 0;
         }
     }
     pub fn damage(&self, target: &Self) -> Health {
@@ -250,50 +366,50 @@ impl Unit {
             AI => match target.unit_type {
                 AI => 1,
                 Hacker => 1,
-                Support => 1,
+                Repair => 1,
                 Tank => 3,
                 Drone => 3,
-                Android => 3,
+                Soldier => 3,
             },
             Hacker => match target.unit_type {
                 AI => 4,
                 Hacker => 1,
-                Support => 2,
+                Repair => 2,
                 Tank => 1,
                 Drone => 1,
-                Android => 1,
+                Soldier => 1,
             },
-            Support => match target.unit_type {
+            Repair => match target.unit_type {
                 AI => 0,
                 Hacker => 1,
-                Support => 1,
+                Repair => 1,
                 Tank => 0,
                 Drone => 0,
-                Android => 0,
+                Soldier => 0,
             },
             Tank => match target.unit_type {
                 AI => 1,
                 Hacker => 1,
-                Support => 1,
+                Repair => 1,
                 Tank => 2,
                 Drone => 2,
-                Android => 3,
+                Soldier => 3,
             },
             Drone => match target.unit_type {
                 AI => 1,
                 Hacker => 1,
-                Support => 1,
+                Repair => 1,
                 Tank => 6,
                 Drone => 2,
-                Android => 1,
+                Soldier => 1,
             },
-            Android => match target.unit_type {
+            Soldier => match target.unit_type {
                 AI => 2,
                 Hacker => 2,
-                Support => 1,
+                Repair => 1,
                 Tank => 2,
                 Drone => 5,
-                Android => 2,
+                Soldier => 2,
             },
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use ai_wargame::{Game, UnitType, Unit, Cell};
 fn main() {
     let mut game = Game::default();
 
-    game[(3,4)] = Cell::Unit{player: game.player(),unit: Unit::new(UnitType::AI)};
+    game[(3,4)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::AI)};
+    game[(4,5)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::Hacker)};
+    game[(6,5)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::Repair)};
+    game[(2,5)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::Tank)};
+    game[(0,5)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::Soldier)};
+    game[(1,1)] = Cell::Unit{player: game.next_player(),unit: Unit::new(UnitType::Drone)};
 
     loop {
         println!("{}",game);
         if let Some((from,to)) = game.get_move_from_stdin() {
             if game.move_unit(from, to) {
+                game.resolve_conflicts();
+                game.remove_dead();
                 game.next_player();
             } else {
                 println!("Invalid move!");
